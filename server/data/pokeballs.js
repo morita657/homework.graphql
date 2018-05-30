3bc515decaf329882489a97ea8740d7d7af5a69b
morita657 <morita657@gmail.com>
2018-05-31 02:51:38 +0900

    Crete pokeball data file

---
+++ server/data/pokeballs.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Pokeball",
+    catch_rate: "1X",
+  },
+  {
+    id: 2,
+    name: "Great Ball",
+    catch_rate: "1.5X",
+  },
+  {
+    id: 3,
+    name: "Ultra Ball",
+    catch_rate: "2X",
+  },
+  {
+    id: 4,
+    name: "Master Bal",
+    catch_rate: "Never Fails",
+  },
+  {
+    id: 5,
+    name: "Drive Ball",
+    catch_rate: "3.5X When In Water",
+  },
+];
