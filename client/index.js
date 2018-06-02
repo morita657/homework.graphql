1371866dc35685c4fb4ce36d933998764ad7cc2f
morita657 <morita657@gmail.com>
2018-06-02 22:46:30 +0900

    Move files

--- client/index.js
+++
@@ -1,8 +0,0 @@
-import { createApolloFetch } from "apollo-fetch";
-
-const uri = "http://api.githunt.com/graphql";
-const apolloFetch = createApolloFetch({ uri });
-apolloFetch.then((data) => {
-  console.log(data);
-  return data;
-});
